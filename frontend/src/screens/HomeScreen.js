cfa306e6a2a0770d890df55c5fc1ea3122b0ae1c
fitsum legesse <fitsumlegesse@icloud.com>
2021-02-25 23:39:40 -0800

    Adds images components and rendors a few products to the client

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Products from '../components/Product'
+import products from '../products'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest products</h1>
+            <Row>
+                {products.map(product=>(
+
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Products product={product} />
+                    </Col>
+                ))}
+
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
