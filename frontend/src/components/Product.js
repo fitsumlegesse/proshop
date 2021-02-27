497e18d730d75d263adf1b937a894cac5f5b9a9c
fitsum legesse <fitsumlegesse@icloud.com>
2021-02-27 02:22:45 -0800

    added components

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = (props) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${props.product._id}`}>
+            <Link to={`/product/${props.product._id}`}>
                 <Card.Img src={props.product.image} varient='top'/>
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${props.product._id}`}>
+                <Link to={`/product/${props.product._id}`}>
                 <Card.Title as='div'>
                     <strong>
                         {props.product.name}
                     </strong>
                 </Card.Title>
-                </a>
+                </Link>
                 <Card.Text as='div'>
-                    <div className='my-3'>
-                        {props.product.rating} from {props.product.numReviews} reviews
-                    </div>
+                    <Rating 
+                        value={props.product.rating}
+                        text={`${props.product.numReviews} reviews`}
+                    />
                 </Card.Text>
             <Card.Text as='h3'>${props.product.price}</Card.Text>
             </Card.Body>
