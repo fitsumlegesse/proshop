0233627e500bc687de757d445c3242f6f904b15c
fitsum legesse <fitsumlegesse@icloud.com>
2021-03-01 15:33:02 -0800

    modifeis productModel and orderModel

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -1,28 +1,67 @@
 import mongoose from 'mongoose'
 
-const productSchema = mongoose.Schema({
+const reviewSchema = mongoose.Schema({
+    name:{type: String, required: true},
+    rating:{type: Number, required: true},
+    comment:{type: String, required: true},
+}, {
+    timestamps: true
+})
+
+const productSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+
     name: {
         type: String,
         required: true
     },
-    email: {
+    image: {
         type: String,
         required: true,
-        unique: true
+       
     },
-    password: {
+    brand: {
         type: String,
         required: true
     },
-    isAdmin: {
-        type: Boolean,
+    category: {
+        type: String,
         required: true,
-        default: false
-    }
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    reviews:[reviewSchema],
+    rating: {
+        type: Number,
+        required: true,
+        default:0
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default:0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default:0
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default:0
+    },
 }{
     timestamps: true
 })
 
-const User = mongoose.model('User', userSchema)
+const Product = mongoose.model('Prodcut', productSchema)
 
-export default User
+export default Product
