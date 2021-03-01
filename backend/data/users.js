d28598c8f4ae6d56c3ae397b51877ed53a7d08f9
fitsum legesse <fitsumlegesse@icloud.com>
2021-03-01 15:51:40 -0800

    adds bcrypt and dummy user data

---
+++ backend/data/users.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    }, 
+
+    {
+        name: 'John Doe',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        
+    },   
+    {
+        name: 'Fitsum Legesse',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        
+    }
+
+]
+
+export default users
