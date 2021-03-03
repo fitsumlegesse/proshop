d8c7fd50d63f73b3e1812d583d90c3708f78bb2b
fitsum legesse <fitsumlegesse@icloud.com>
2021-03-02 21:21:33 -0800

    adds an action and reducer folder

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,18 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {productListReducer} from './reducers/productReducers'
 
-const reducer = combineReducers()
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+
 const initialState = {}
 const middleWare = [thunk]
 const store = createStore(
     reducer,
     initialState,
-    composeWithDevTools(applyMiddleware([...middleWare]))
+    composeWithDevTools(applyMiddleware(...middleWare))
 
 )
 
