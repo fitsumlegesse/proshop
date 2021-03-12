52e62ad1b5b6adaa316f38f6ff0e122f572ecb14
fitsum legesse <fitsumlegesse@icloud.com>
2021-03-11 17:30:29 -0800

    adds a mechanism to register a user and add it to the user database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,6 +32,50 @@ const authUser = asyncHandler( async (req,res)=>{
    }
 })
 
+//@Auth register a user 
+//@route  POST /api/users
+//@access  public 
+
+const registerUser = asyncHandler( async (req,res)=>{
+    
+   const { name,email, password} = req.body
+
+   const userExists = await User.findOne({email})
+   
+   if(userExists)
+   {
+       res.status(400)
+       throw new Error('User already exists')
+   }
+
+   const user = await User.create(
+       {
+           name,
+           email,
+           password
+       })
+
+       if(user)
+       {
+           res.status(201).json
+           (
+               {
+                _id: user._id,
+                name:user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id)
+
+               }
+           )}else{
+               res.status(400)
+               throw new Error('Invalid user data')
+           }
+   
+})
+
+
+
 //@Auth get user profile 
 //@route  GET /api/users/profile
 //@access  Private
@@ -57,4 +101,4 @@ const getUserProfile = asyncHandler( async (req,res)=>{
 
 
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
