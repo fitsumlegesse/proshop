839323198f424797f883c73827440f446108570c
fitsum legesse <fitsumlegesse@icloud.com>
2021-03-17 00:26:16 -0700

    creates a mechanism to register users and log them in

--- frontend/src/constants/userConstants.js
+++ frontend/src/constants/userConstants.js
@@ -2,3 +2,7 @@ export const  USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST'
 export const  USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const  USER_LOGIN_FAIL = 'USER_LOGIN_FAIL'
 export const  USER_LOGOUT = 'USER_LOGOUT'
+
+export const  USER_Register_REQUEST = 'USER_Register_REQUEST'
+export const  USER_Register_SUCCESS = 'USER_Register_SUCCESS'
+export const  USER_Register_FAIL = 'USER_Register_FAIL'
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS,USER_LOGIN_FAIL,USER_LOGOUT} from '../constants/userConstants'
+import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS,USER_LOGIN_FAIL,USER_LOGOUT, USER_Register_REQUEST, USER_Register_SUCCESS, USER_Register_FAIL} from '../constants/userConstants'
 
 export const userLoginReducer = (state = { }, action)=>{
     switch (action.type){
@@ -10,6 +10,20 @@ export const userLoginReducer = (state = { }, action)=>{
             return { loading: false, error: action.payload}
         case USER_LOGOUT:
             return {}
+        default:
+            return state
+        
+    }
+}
+
+export const userRegisterReducer = (state = { }, action)=>{
+    switch (action.type){
+        case USER_Register_REQUEST:
+            return {loading: true}
+        case USER_Register_SUCCESS:
+            return { loading: false, userInfo: action.payload}
+        case USER_Register_FAIL:
+            return { loading: false, error: action.payload}
         default:
             return state
         
